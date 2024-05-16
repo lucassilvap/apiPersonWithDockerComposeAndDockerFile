949e54b333c3b800702ee147aab79cf8b6a80805
Anderson Oliveira <andervilo@hotmail.com>
2024-05-16 19:08:18 -0300

    correção docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM openjdk:21-jdk-slim
 COPY target/apiPerson-0.0.1-SNAPSHOT.jar /app/app.jar
-EXPOSE 9090
 CMD ["java", "-jar", "/app/app.jar"]
--- src/main/java/com/example/apiPerson/controller/PersonController.java
+++ src/main/java/com/example/apiPerson/controller/PersonController.java
@@ -1,10 +1,15 @@
 package com.example.apiPerson.controller;
 
-import com.example.apiPerson.entity.PersonEntity;
-import com.example.apiPerson.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.apiPerson.entity.PersonEntity;
+import com.example.apiPerson.service.PersonService;
 
 @RestController
 public class PersonController
@@ -17,8 +22,8 @@ public class PersonController
         personService.savePerson(personEntity);
         return ResponseEntity.ok("salvo com sucesso");
     }
-    @GetMapping("{id}")
-    public ResponseEntity<?> findPersonById(@RequestParam(name = "id") Long id){
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findPersonById(@PathVariable Long id){
         return ResponseEntity.ok(personService.getPerson(id));
     }
 }
